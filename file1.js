1e6007b9bfded56bf6dee4f7e4d20fa9b3ec8c76
Дмитрий <1532511@mail.ru>
2019-10-01 20:01:10 +0300

    add feature 3 in branch feature3

--- file1.js
+++ file1.js
@@ -1,6 +1,6 @@
 var test = function () {
 	var change = 'create some changes ';
-	// thats from branch add-feature-1
+	// thats from branch add-feature-3 and change it in branch feature-3
 
 	//another changes at same line but in master branch!!! 
 
